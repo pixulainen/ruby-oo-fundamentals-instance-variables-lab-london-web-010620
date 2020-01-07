709642d87ce8fc2f9449c84756ac0ef0e4994eaf
George Apetrei <george.apetrei@gmail.com>
2020-01-07 11:55:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog 
   def name=(dogs_name)
-    @this_dog_name = dogs_name
+    @this_dogs_name = dogs_name
   end
   
   def name 
-    @this_dog_name
+    @this_dogs_name
   end
 end
 lassie = Dog.new
